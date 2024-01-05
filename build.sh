c6692b92c933cb51674052d78d2b88b729d34633
Lauquik <laukik.22010776@viit.ac.in>
2024-01-05 10:48:13 +0530

    nginx

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 export PATH="$PATH:$(go env GOPATH)/bin"
 rm -Rf generated
 mkdir generated
